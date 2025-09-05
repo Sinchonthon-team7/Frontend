1752b787b4783ca29eebe0eb33860323c0870f5d
YJ <hghk0046@gmail.com>
2025-09-05 23:12:13 +0900

    feat: 헤더 완성, 회원가입페이지 구현

--- src/assets/Pages/MainPage.jsx
+++ src/assets/Pages/MainPage.jsx
@@ -1,7 +1,7 @@
 export const MainPage = () => {
     return(
         <div className="mx-auto w-full">
-            <h1>메인 페이지입니다.</h1>
+            <div className=""></div>
         </div>
     )
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -10,25 +10,33 @@ export const Header = () => {
   const goToScamIs = () => {
     navigate('/scamis');
   };
-  
-  const goToLogin = () => {
-    navigate('/login')
-  }
 
+  const goToLogin = () => {
+    navigate('/login');
+  };
+
+  const goToSignUp = () => {
+    navigate('/signup');
+  }
+  
   return (
-    <div className="flex w-full h-[60px] px-[16px] bg-[#8180F7] items-center">
+    <div className="flex w-full h-[60px] px-[16px] bg-[#8180F7] items-center justify-between pr-30">
       <div className="w-[40px] h-[40px]">{/* 로고 넣기*/}</div>
 
       {/* 헤더 */}
-      <div className="flex gap-10">
-        <div className="w-[100px] cursor-pointer" onClick={goToIsScam}>
+      <div className="flex gap-15">
+        <div className="w-auto cursor-pointer" onClick={goToIsScam}>
           <p className="text-white text-[20px] leading-1.5">사칭일까요?</p>
         </div>
-        <div className="w-[100px] cursor-pointer" onClick={goToScamIs}>
+        <div className="w-auto cursor-pointer" onClick={goToScamIs}>
           <p className="text-white text-[20px] leading-1.5">사칭이에요</p>
         </div>
-        <div className="w-[100px] cursor-pointer" onClick={goToLogin}>
-          <p className="text-white text-[20px] leading-1.5">사칭이에요</p>
+        <div className="w-auto cursor-pointer" onClick={goToLogin}>
+          <p className="text-white text-[16px] leading-1.5">로그인</p>
+        </div>
+
+        <div className="w-auto cursor-pointer" onClick={goToSignUp}>
+          <p className="text-white text-[16px] leading-1.5">회원가입</p>
         </div>
       </div>
     </div>
