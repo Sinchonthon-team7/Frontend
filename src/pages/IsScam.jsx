bf04865e1310207b0cca105480da24f75512f95c
이윤서 <yyoo04@naver.com>
2025-09-06 04:14:12 +0900

    fix : IsScam UI 수정

--- src/pages/IsScam.jsx
+++ src/pages/IsScam.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from 'react-router-dom';
-import ListTable from '../../components/ListTable';
-import { isScamData } from '../../data/isscam';
+import ListTable from '../components/ListTable';
+import { isScamData } from '../data/isscam';
 
 const IsScam = () => {
   const nav = useNavigate();
   return (
-    <div className="flex flex-col m-[48px] items-center">
+    <div className="flex flex-col m-[48px] items-center font-sans">
       {/* 헤더 */}
       <div className="flex w-full justify-between items-baseline ">
         <div className="flex gap-[16px] items-baseline">
@@ -21,6 +21,26 @@ const IsScam = () => {
           >
             글쓰기
           </button>
+        </div>
+      </div>
+      <div className="font-sans flex flex-row gap-[16px] judstify-start w-full mt-[30px]">
+        <div className="bg-white shadow-md rounded-[50px] px-[12px]">
+          카테고리
+        </div>
+        <div className="bg-white shadow-md rounded-[50px] px-[12px]">
+          카테고리
+        </div>
+        <div className="bg-white shadow-md rounded-[50px] px-[12px]">
+          카테고리
+        </div>
+        <div className="bg-white shadow-md rounded-[50px] px-[12px]">
+          카테고리
+        </div>
+        <div className="bg-white shadow-md rounded-[50px] px-[12px]">
+          카테고리
+        </div>
+        <div className="bg-white shadow-md rounded-[50px] px-[12px]">
+          카테고리
         </div>
       </div>
       <div className="w-full mt-[30px]">
--- src/pages/IsScamWrite.jsx
+++ src/pages/IsScamWrite.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom';
 const IsScamWrite = () => {
   const nav = useNavigate();
   return (
-    <div className="m-[48px] font-sans">
-      <div className="flex justify-start items-center gap-[16px]">
+    <div className="p-[48px] font-sans">
+      <div className="flex justify-start items-center gap-[22px]">
         <img
           src="/back.svg"
           className="w-[30px] aspect-square hover:cursor-pointer"
@@ -12,33 +12,63 @@ const IsScamWrite = () => {
         />
         <div className="font-bold text-[32px]">이거 사칭일까요?</div>
       </div>
-      <div className="bg-[#F8F8F8] h-dvh w-full mt-[30px] py-[48px] px-[120px] flex gap-[42px]">
-        <div>캐릭터</div>
+      <div className="bg-[#F8F8F8] w-full mt-[30px] py-[48px] px-[120px] flex gap-[42px] rounded-[20px] shadow-md">
+        <div className="flex-none shrink-0 w-[90px] h-[90px] overflow-hidden rounded-full border border-gray-300">
+          <img
+            alt="캐릭터"
+            src="/agu.jpg"
+            className="w-full h-full object-cover"
+          />
+        </div>
         <div className="w-full h-full gap-[16px] flex flex-col">
-          <div>카테고리</div>
-          <div className="text-[32px] font-bold">제목을 작성해주세요</div>
+          <div className="flex gap-[16px]">
+            {' '}
+            <div className="bg-[#EFEFEF] rounded-[50px] px-[12px] hover:cursor-pointer">
+              카테고리
+            </div>
+            <div className="bg-[#EFEFEF] rounded-[50px] px-[12px] hover:cursor-pointer">
+              카테고리
+            </div>
+            <div className="bg-[#EFEFEF] rounded-[50px] px-[12px] hover:cursor-pointer">
+              카테고리
+            </div>
+            <div className="bg-[#EFEFEF] rounded-[50px] px-[12px] hover:cursor-pointer">
+              카테고리
+            </div>
+            <div className="bg-[#EFEFEF] rounded-[50px] px-[12px] hover:cursor-pointer">
+              카테고리
+            </div>
+            <div className="bg-[#EFEFEF] rounded-[50px] px-[12px] hover:cursor-pointer">
+              카테고리
+            </div>
+          </div>
+          <input
+            className="bg-[#EFEFEF] p-[12px] rounded-[10px] text-[32px] font-bold text-black"
+            placeholder="제목을 작성해주세요."
+          />
           <div>
             {/* 사진/영상 첨부 */}
             <div className="text-[12px] text-gray-600 mb-[8px]">
               사진/영상 첨부
             </div>
-            <div className="bg-white w-[70px] h-[80px] rounded-[10px] flex justify-center">
+            <div className="bg-[#EFEFEF] w-[70px] h-[80px] rounded-[10px] flex justify-center">
               <img
                 src="/plus.svg"
-                className="w-[20px]  justify-center items-center aspect-square hover:cursor-pointer"
+                className="w-[20px] justify-center items-center aspect-square hover:cursor-pointer"
               />
             </div>
           </div>
           <div>
             {/* 글 작성 */}
-            <div>글 작성</div>
-            <div className="bg-gray-200 p-[20px] h-[480px]">
-              내용을 작성해주세요
-            </div>
+            <div className="mb-[10px] font-black">사칭이 으심되.</div>
+            <textarea
+              placeholder="내용을 작성해주세요."
+              className="bg-[#EFEFEF] flex items-start p-[20px] w-full h-[480px]"
+            />
           </div>
           <div className="flex justify-end items-center">
             <button
-              className="bg-primary w-[98px] h-[32px] rounded-[20px] text-white px-[2px]"
+              className="bg-secondary w-[98px] h-[32px] rounded-[20px] text-white px-[2px] hover:cursor-pointer"
               onClick={() => alert('등록완료!')}
             >
               등록하기
